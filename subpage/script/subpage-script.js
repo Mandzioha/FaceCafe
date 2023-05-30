a019715ef38d703b32a1977c5baf1e0e72bb0c2b
Bartosz Pudo <pudobartosz95@gmail.com>
2023-05-30 15:05:00 +0200

    Added Subpage for Homework & Alive 97

---
+++ subpage/script/subpage-script.js
@@ -0,0 +1,27 @@
+const navstrap = document.querySelector('#navstrap');
+
+window.addEventListener('scroll', ()=> {
+    if (window.scrollY >= 90) {
+      console.log("Użytkownik przescrollował w dół o 90px.");
+      navstrap.classList.add('hide');
+      navstrap.classList.add('hidden');
+      console.log('test');
+    }else{
+        navstrap.classList.remove('hide');
+        navstrap.classList.remove('hidden');
+    }
+  });
+
+  var previousScrollPosition = window.scrollY;
+
+window.addEventListener('scroll', function() {
+  var currentScrollPosition = window.scrollY;
+
+  if (currentScrollPosition < previousScrollPosition) {
+    console.log("Użytkownik zaczął przewijać stronę do góry.");
+    navstrap.classList.remove('hide');
+        navstrap.classList.remove('hidden');
+}
+
+  previousScrollPosition = currentScrollPosition;
+});
